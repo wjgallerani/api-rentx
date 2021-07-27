b46e56e09cb0d1d05b7db804704cc6536775b37f
William Gallerani <williamglrn@gmail.com>
2021-07-26 23:41:33 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
 import "express-async-errors"; // yarn add express-async-errors
+import cors from "cors";
 
 import express, { Request, Response, NextFunction } from "express";
 
@@ -26,6 +27,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 //Middleware de Erro
